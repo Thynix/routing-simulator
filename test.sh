e31a1c25b96f838701c81e53ffce793538f76ca2
Steve Dougherty <steve@asksteved.com>
2012-05-03 23:10:01 -0400

    Parallel simulation using GNU parallel.

--- test.sh
+++ test.sh
@@ -24,11 +24,14 @@ simulation="--probe 50"
 
 #TODO: Some way to not repeat directory names, or generate them from simulation parameters. Perhaps Python would be better-suited?
 
-java -jar "$jar" $degree --size "$size" --metropolis-hastings -D "degree_mh_$size/peerDist.dat" -L "degree_mh_$size/links_output" -O "degree_mh_$size/occurenceDistribution/" $simulation
-java -jar "$jar" $degree --size "$size" -D "degree_uniform_$size/peerDist.dat" -L "degree_uniform_$size/links_output" -O "degree_uniform_$size/occurenceDistribution/" $simulation
+echo $degree --size "$size" --metropolis-hastings -D "degree_mh_$size/peerDist.dat" -L "degree_mh_$size/links_output" -O "degree_mh_$size/occurenceDistribution/" $simulation > test.config
+echo $degree --size "$size" -D "degree_uniform_$size/peerDist.dat" -L "degree_uniform_$size/links_output" -O "degree_uniform_$size/occurenceDistribution/" $simulation >> test.config
+echo $ideal --size "$size" --metropolis-hastings -D "ideal_mh_$size/peerDist.dat" -L "ideal_mh_$size/links_output" -O "ideal_mh_$size/occurenceDistribution/" $simulation >> test.config
+echo $ideal --size "$size" -D "ideal_uniform_$size/peerDist.dat" -L "ideal_uniform_$size/links_output" -O "ideal_uniform_$size/occurenceDistribution/" $simulation >> test.config
 
-java -jar "$jar" $ideal --size "$size" --metropolis-hastings -D "ideal_mh_$size/peerDist.dat" -L "ideal_mh_$size/links_output" -O "ideal_mh_$size/occurenceDistribution/" $simulation
-java -jar "$jar" $ideal --size "$size" -D "ideal_uniform_$size/peerDist.dat" -L "ideal_uniform_$size/links_output" -O "ideal_uniform_$size/occurenceDistribution/" $simulation
+#TODO: This breaks on paths with spaces because -C uses a space as a column separator for different arguments.
+#Uses http://www.gnu.org/software/parallel/
+parallel -C " " java -jar "$jar" < test.config
 
 for dir in "degree_mh_$size" "degree_uniform_$size" "ideal_mh_$size" "ideal_uniform_$size"
 do
