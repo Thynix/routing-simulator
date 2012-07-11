055a324370576c87ce3a8ae31cbab531e52fb935
Steve Dougherty <steve@asksteved.com>
2012-07-11 17:21:24 -0400

    Use single imports instead of wildcard.

--- src/main/java/org/freenetproject/routing_simulator/WeightedDistribution.java
+++ src/main/java/org/freenetproject/routing_simulator/WeightedDistribution.java
@@ -1,6 +1,10 @@
 package org.freenetproject.routing_simulator;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
